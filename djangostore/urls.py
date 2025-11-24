323a09bb148a5cc4e893003f73768614ec7f70a5
Geli033 <ehlop297@gmail.com>
2025-11-24 17:29:17 -0600

    Corrección

--- djangostore/urls.py
+++ djangostore/urls.py
@@ -4,6 +4,3 @@ from django.conf.urls.static import static
 urlpatterns = [
     # Aquí tus rutas
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
